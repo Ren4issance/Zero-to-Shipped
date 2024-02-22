bfbd75aadd536b91f3187021db71183ab113d0bf
Ren4issance <antoine.arnaud63@gmail.com>
2024-02-23 00:55:29 +0100

    use page extension

--- eventio/src/pages/team.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { BlitzPage } from "@blitzjs/next";
-
-export const TeamPage: BlitzPage = () => {
-  return <div>this is about our team</div>;
-};
-
-export default TeamPage;
