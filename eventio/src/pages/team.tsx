45faa66bedbb45570a442a745bc6b820a6bd9cb2
Ren4issance <antoine.arnaud63@gmail.com>
2024-02-23 00:35:06 +0100

    Update project configuration and add Hello World message

---
+++ eventio/src/pages/team.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { BlitzPage } from "@blitzjs/next";
+
+export const TeamPage: BlitzPage = () => {
+  return <div>this is about our team</div>;
+};
+
+export default TeamPage;
